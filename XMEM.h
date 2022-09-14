83495cb41a4557c2ef770d5db111fad9ec45e644
Viktor Naas <viktorn@stud.ntnu.no>
2022-09-14 14:42:00 +0200

    Read and write functions. Need error handling

--- XMEM.h
+++ XMEM.h
@@ -1,6 +1,13 @@
 #ifndef XMEM_H
 #define XMEM
 
-void XMEM_init()
+#define XMEM_BASE 0x1800
+#define XMEM_SIZE 0x800
+
+void XMEM_init();
+
+void XMEM_write(uint8_t data, uint16_t addr);
+
+uint8_t XMEM_read(uint16_t addr);
 
 #endif
