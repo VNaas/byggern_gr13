f1a9689833b151f3c58a17af8645f2aef5828694
Viktor Naas <viktorn@stud.ntnu.no>
2022-09-21 14:47:37 +0200

    .

--- main.c
+++ main.c
@@ -10,7 +10,44 @@
 #include <util/delay.h>
 #include "uart_driver.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include "ADC_driver.h"
+#include "XMEM.h"
+
+void SRAM_test(void)
+{
+	volatile char *ext_ram = (char *) 0x1800; // Start address for the SRAM
+	uint16_t ext_ram_size = 0x800;
+	uint16_t write_errors = 0;
+	uint16_t retrieval_errors = 0;
+	printf("Starting SRAM test...\n");
+	// rand() stores some internal state, so calling this function in a loop will
+	// yield different seeds each time (unless srand() is called before this function)
+	uint16_t seed = rand();
+	// Write phase: Immediately check that the correct value was stored
+	srand(seed);
+	for (uint16_t i = 0; i < ext_ram_size; i++) {
+		uint8_t some_value = rand();
+		ext_ram[i] = some_value;
+		uint8_t retreived_value = ext_ram[i];
+		if (retreived_value != some_value) {
+			printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
+			write_errors++;
+		}
+	}
+	// Retrieval phase: Check that no values were changed during or after the write phase
+	srand(seed);
+	// reset the PRNG to the state it had before the write phase
+	for (uint16_t i = 0; i < ext_ram_size; i++) {
+		uint8_t some_value = rand();
+		uint8_t retreived_value = ext_ram[i];
+		if (retreived_value != some_value) {
+			printf("Retrieval phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
+			retrieval_errors++;
+		}
+	}
+	printf("SRAM test completed with \n%4d errors in write phase and \n%4d errors in retrieval phase\n\n", write_errors, retrieval_errors);
+}
 
 void latch_test(void){
 	/* --------------latch test:--------------------*/
@@ -28,22 +65,8 @@ void latch_test(void){
 
 int main(void)
 {
-	/* Turn on LED in PA0 */
-	DDRA |= 0b11111111;
-	PORTA |= 0b00000001;
-
-	/*latch enabling*/
-	// PORTE |= 0b0; //Adress Latch Enable set to low
-
-	// while (1)
-    // {}
-
-
-
-	// FILE* mystream = fdevopen(&USART_transmit, &USART_receive)
 	USART_init(MYUBRR);
-	// stdout = &uart_str;
-	// unsigned char myResponse = USART_receive();
+	XMEM_init();
 	while(1){
 			ADC_start_conversion();
 			struct ADC_data data = ADC_get_data();
