febafb085b27170d458cc0c4a76729648b7169f1
Viktor Naas <viktorn@stud.ntnu.no>
2022-10-11 21:20:00 +0200

    function declarations...

---
+++ MCP2515_driver.h
@@ -0,0 +1,13 @@
+#include "mcp2515.h"
+
+char mcp2515_read();
+
+void mcp2515_write();
+
+void mcp2515_request_to_send();
+
+void mcp2515_bit_modify();
+
+void mcp2515_reset();
+
+char mcp2515_read_status();
